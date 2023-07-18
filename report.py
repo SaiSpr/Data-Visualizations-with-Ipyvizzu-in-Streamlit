e4717f995fbee48215dfb5ab3a0e64f177ac40d8
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-07-19 00:08:11 +0530

    Update report.py

--- report.py
+++ report.py
@@ -1,38 +1,62 @@
 from ipyvizzu import Data, Config
-from ipyvizzustory import Story, Slide, Step
+from ipyvizzustory import Slide, Step
+ 
+from ipyvizzustory import Story  # or
+ 
+# from ipyvizzustory.env.st.story import Story
  
  
-# Create data object
+# create data and initialize Story with the created data
+ 
 data = Data()
 data.add_series("Foo", ["Alice", "Bob", "Ted"])
 data.add_series("Bar", [15, 32, 12])
 data.add_series("Baz", [5, 3, 2])
  
+# you can also add data with pandas
  
-# Create story object, add data to it
+# import pandas as pd
+#
+# data = Data()
+# df = pd.read_csv(
+#     "https://ipyvizzu-story.vizzuhq.com/0.7/assets/data/data.csv"
+# )
+# data.add_data_frame(df)
+ 
 story = Story(data=data)
  
  
-# Each slide here is a page in the final interactive story
-# Add the first slide
+# create Slides and Steps and add them to the Story
+ 
 slide1 = Slide(
     Step(
         Config({"x": "Foo", "y": "Bar"}),
     )
 )
-# Add the slide to the story
 story.add_slide(slide1)
  
-# Create the second slide
-# Configs provided here are changes to the visualization
-# created in the previous Step
 slide2 = Slide(
-    Step(
-        Config({"color": "Foo", "x": "Baz", "geometry": "circle"}),
-    )
+    Step(Config({"color": "Foo", "x": "Baz", "geometry": "circle"}))
 )
 story.add_slide(slide2)
  
  
-# Play the created story!
+# note: in Streamlit,
+# you need to set the width and height in pixels as int
+ 
+story.set_size(width=800, height=480)
+ 
+ 
+# you can export the Story into a html file
+ 
+story.export_to_html(filename="mystory.html")
+ 
+# or you can get the html Story as a string
+ 
+html = story.to_html()
+print(html)
+ 
+ 
+# you can display the Story with the `play` method
+ 
 story.play()
